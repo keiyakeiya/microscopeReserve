4e5a8199efdc611c3bf9c08dbbe49e32e5a4585e
keiy_ <keiy.keiy.alpha@gmail.com>
2021-09-23 14:54:08 +0900

    fix theme color & change cache name

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,6 @@
 'use strict';
 
-// const CACHE_NAME = 'cache-v1';
-// const CACHE_NAME = 'cache-v2';
-const CACHE_NAME = 'cache-v3';
+const CACHE_NAME = 'mr_cache_v1';
 
 const urlsToCache = [
   './index.html',
